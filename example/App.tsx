841fd7a6ddd51b6ef2d2346e0ae14f6050415075
vlad <vladkalyuzhnyu@gmail.com>
2020-08-27 16:36:30 +0300

    changes

--- example/App.tsx
+++ example/App.tsx
@@ -9,103 +9,52 @@
  */
 
 import React from 'react';
-import {SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View,} from 'react-native';
+import {Alert, Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TextInput, View,} from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-declare const global: { HermesInternal: null | {} };
-
 const App = () => {
-  return (
-      <>
-        <StatusBar barStyle="dark-content"/>
-        <SafeAreaView>
-          <ScrollView
-              contentInsetAdjustmentBehavior="automatic"
-              style={styles.scrollView}>
-            <Header/>
-            {global.HermesInternal == null ? null : (
-                <View style={styles.engine}>
-                  <Text style={styles.footer}>Engine: Hermes</Text>
-                </View>
-            )}
-            <View style={styles.body}>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Step One</Text>
-                <Text style={styles.sectionDescription}>
-                  Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                  this screen and then come back to see your edits.
-                </Text>
-              </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>See Your Changes</Text>
-                <Text style={styles.sectionDescription}>
-                  <ReloadInstructions/>
-                </Text>
-              </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Debug</Text>
-                <Text style={styles.sectionDescription}>
-                  <DebugInstructions/>
-                </Text>
-              </View>
-              <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Learn More</Text>
-                <Text style={styles.sectionDescription}>
-                  Read the docs to discover what to do next:
-                </Text>
-              </View>
-              <LearnMoreLinks/>
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 16}}
+                placeholder="Token"/>
+            <View style={styles.button}>
+                <Button
+                    title="Set token"
+                    onPress={() => Alert.alert('Simple Button pressed')}/>
             </View>
-          </ScrollView>
-        </SafeAreaView>
-      </>
-  );
-};
+            <View style={styles.button}>
+                <Button
+                    title="Toggle"
+                    onPress={() => Alert.alert('Simple Button pressed')}/>
+            </View>
+            <View style={styles.button}>
+                <Button
+                    title="Force update"
+                    onPress={() => Alert.alert('Simple Button pressed')}/>
+            </View>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        flex: 1,
+        display: "flex",
+        flexDirection: "column"
+    },
+    button: {
+        marginStart: 16,
+        marginEnd: 16,
+        marginBottom: 16
+    },
 });
 
 export default App;
