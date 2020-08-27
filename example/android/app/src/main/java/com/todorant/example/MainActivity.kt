20d704c8d9215537b39ea51307f33c4402ab5d76
vlad <vladkalyuzhnyu@gmail.com>
2020-08-27 16:56:46 +0300

    changes

--- example/android/app/src/main/java/com/todorant/example/MainActivity.kt
+++ example/android/app/src/main/java/com/todorant/example/MainActivity.kt
@@ -1,15 +1,12 @@
-package com.todorant.example;
+package com.todorant.example
 
-import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivity
 
-public class MainActivity extends ReactActivity {
+class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "TodorantWidget";
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName() = "TodorantWidget"
 }
